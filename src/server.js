7913cde32c3f2830fa06f63d02c12cb447ee1164
Anderson <andersoninfonet@gmail.com>
2021-04-02 01:08:20 -0300

    add calc for days of work

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const server = express()
 
 server.set('view engine', 'ejs')
 server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
 server.use(routes)
 const port = 3333
 
