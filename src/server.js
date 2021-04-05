e67d612a4f8c5dd2d9a7d89ae4fb3a24a6d726aa
Anderson <andersoninfonet@gmail.com>
2021-04-05 00:33:00 -0300

    ♻️ create controllers and models

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const routes = require('./routes')
+const path = require('path')
 const server = express()
 
 server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'views'))
+
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: true }))
 server.use(routes)
+
 const port = 3333
 
 server.listen(port, console.log(`Server is Up and running on port ${port} ...`))
---
+++ src/utils/JobUtils.js
@@ -0,0 +1,19 @@
+const Profile = require('../models/Profile')
+
+module.exports = {
+  remainingDays(job) {
+    const totalDays = (job['total-hours'] / job['daily-hours']).toFixed()
+
+    const createdDate = new Date(job.created_at)
+    const dueDay = createdDate.getDate() + Number(totalDays)
+    const dueDateInMs = createdDate.setDate(dueDay)
+
+    const timeDiffInMs = dueDateInMs - Date.now()
+
+    const dayInMS = 1000 * 60 * 60 * 24
+    const dayDiff = Math.floor(timeDiffInMs / dayInMS)
+
+    return dayDiff
+  },
+  calculateAmount: (job) => Profile.get()['value-hour'] * job['total-hours']
+}
